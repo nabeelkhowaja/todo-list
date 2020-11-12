6e1c6377593f37f91c33816439ff2175fa193eff
nabeelkhowaja <nabeel.khowaja@ihsinformatics.com>
2020-11-12 22:18:01 +0500

    - Created sign-up screen layout - Enabled view binding

--- app/src/main/java/com/nabeelkhowaja/todolist/view/LoginActivity.kt
+++ app/src/main/java/com/nabeelkhowaja/todolist/view/LoginActivity.kt
@@ -2,11 +2,24 @@ package com.nabeelkhowaja.todolist.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.nabeelkhowaja.todolist.R
+import com.nabeelkhowaja.todolist.databinding.ActivityLoginBinding
+import com.nabeelkhowaja.todolist.viewmodel.LoginViewModel
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var loginViewModel: LoginViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loginViewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+
+
     }
 }
---
+++ app/src/main/java/com/nabeelkhowaja/todolist/view/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.nabeelkhowaja.todolist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nabeelkhowaja.todolist.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
