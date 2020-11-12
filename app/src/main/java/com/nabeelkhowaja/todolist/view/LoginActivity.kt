4607b8f8be746b213274baf6dd07f4a1a4679b5b
nabeelkhowaja <nabeel.khowaja@ihsinformatics.com>
2020-11-12 07:57:37 +0500

    - Created login screen layout

---
+++ app/src/main/java/com/nabeelkhowaja/todolist/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.nabeelkhowaja.todolist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nabeelkhowaja.todolist.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
