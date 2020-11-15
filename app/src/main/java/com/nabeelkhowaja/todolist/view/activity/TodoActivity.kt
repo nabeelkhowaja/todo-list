2f307e54684480c112bcd4f1639740c35aa3329a
nabeelkhowaja <nabeel.khowaja@ihsinformatics.com>
2020-11-15 17:35:04 +0500

    - Implemented logout functionality - wip - Todo feature

--- app/src/main/java/com/nabeelkhowaja/todolist/view/activity/TodoActivity.kt
+++ app/src/main/java/com/nabeelkhowaja/todolist/view/activity/TodoActivity.kt
@@ -1,12 +1,60 @@
 package com.nabeelkhowaja.todolist.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.nabeelkhowaja.todolist.R
+import com.nabeelkhowaja.todolist.databinding.ActivityEntryBinding
+import com.nabeelkhowaja.todolist.databinding.ActivityTodoBinding
 
 class TodoActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityTodoBinding? = null
+    private val binding get() = mBinding!! //overriding getter of mBinding object to avoid handling nullability
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_todo)
+        mBinding = ActivityTodoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false);
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_logout -> logout()
+        }
+        return false
+    }
+
+    private fun logout() {
+        MaterialAlertDialogBuilder(
+            this,
+            R.style.MyThemeOverlay_MaterialComponents_MaterialAlertDialog
+        )
+            .setTitle("Alert")
+            .setMessage("Do you want to logout?")
+            .setCancelable(false)
+            .setPositiveButton("Yes") { dialog, _ ->
+                val intent = Intent(this@TodoActivity, EntryActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+                this@TodoActivity.finish()
+                dialog.dismiss()
+            }
+            .setNegativeButton("No") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 }
